070d8597f9b4f274440b3c69d9d672e06ca7ce1e
Tamas Csonka <tamas.csonka@ibm.com>
2022-07-06 22:06:39 +0200

    use resource instead of data

--- internal/providers/terraform/ibm/testdata/ibm_cos_bucket_test/ibm_cos_bucket_test.tf
+++ internal/providers/terraform/ibm/testdata/ibm_cos_bucket_test/ibm_cos_bucket_test.tf
@@ -11,13 +11,13 @@ provider "ibm" {
     region = "us-south"
 }
 
-data "ibm_resource_group" "cos_group" {
+resource "ibm_resource_group" "cos_group" {
   name = "cos-resource-group"
 }
 
 resource "ibm_resource_instance" "cos_instance" {
   name              = "cos-instance"
-  resource_group_id = data.ibm_resource_group.cos_group.id
+  resource_group_id = ibm_resource_group.cos_group.id
   service           = "cloud-object-storage"
   plan              = "standard"
   location          = "global"
