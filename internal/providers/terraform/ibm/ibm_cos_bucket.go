98ca46b15d5d53e428fd783885c73d9d0d7f164a
Tamas Csonka <tamas.csonka@ibm.com>
2022-07-06 15:08:12 +0200

    feat(ibm): add cos bucket resource

---
+++ internal/providers/terraform/ibm/ibm_cos_bucket.go
@@ -0,0 +1,24 @@
+package ibm
+
+import (
+	"github.com/infracost/infracost/internal/resources/ibm"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+func getIbmCosBucketRegistryItem() *schema.RegistryItem {
+	return &schema.RegistryItem{
+		Name:  "ibm_ibm_cos_bucket",
+		RFunc: newIbmCosBucket,
+	}
+}
+
+func newIbmCosBucket(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
+	region := d.Get("region").String()
+	r := &ibm.IbmCosBucket{
+		Address: d.Address,
+		Region:  region,
+	}
+	r.PopulateUsage(u)
+
+	return r.BuildResource()
+}
---
+++ internal/resources/ibm/ibm_cos_bucket.go
@@ -0,0 +1,44 @@
+package ibm
+
+import (
+	"github.com/infracost/infracost/internal/resources"
+	"github.com/infracost/infracost/internal/schema"
+)
+
+// IbmCosBucket struct represents <TODO: cloud service short description>.
+//
+// <TODO: Add any important information about the resource and links to the
+// pricing pages or documentation that might be useful to developers in the future, e.g:>
+//
+// Resource information: https://cloud.ibm.com/<PATH/TO/RESOURCE>/
+// Pricing information: https://cloud.ibm.com/<PATH/TO/PRICING>/
+type IbmCosBucket struct {
+	Address string
+	Region  string
+}
+
+// IbmCosBucketUsageSchema defines a list which represents the usage schema of IbmCosBucket.
+var IbmCosBucketUsageSchema = []*schema.UsageItem{
+}
+
+// PopulateUsage parses the u schema.UsageData into the IbmCosBucket.
+// It uses the `infracost_usage` struct tags to populate data into the IbmCosBucket.
+func (r *IbmCosBucket) PopulateUsage(u *schema.UsageData) {
+	resources.PopulateArgsWithUsage(r, u)
+}
+
+// BuildResource builds a schema.Resource from a valid IbmCosBucket struct.
+// This method is called after the resource is initialised by an IaC provider.
+// See providers folder for more information.
+func (r *IbmCosBucket) BuildResource() *schema.Resource {
+	costComponents := []*schema.CostComponent{
+		// TODO: add cost components
+	}
+
+	return &schema.Resource{
+		Name:           r.Address,
+		UsageSchema:    IbmCosBucketUsageSchema,
+		CostComponents: costComponents,
+	}
+}
+
