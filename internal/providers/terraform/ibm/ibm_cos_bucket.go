c50cd32b67ac8eb73dd01662c5102b3394fc9d43
Tamas Csonka <tamas.csonka@ibm.com>
2022-07-13 17:07:13 +0200

    retrieve location, specify storage class, make reference via resource_instance_id

--- internal/providers/terraform/ibm/ibm_cos_bucket.go
+++ internal/providers/terraform/ibm/ibm_cos_bucket.go
@@ -1,22 +1,46 @@
 package ibm
 
 import (
+	"fmt"
+
 	"github.com/infracost/infracost/internal/resources/ibm"
 	"github.com/infracost/infracost/internal/schema"
+	"github.com/tidwall/gjson"
 )
 
 func getIbmCosBucketRegistryItem() *schema.RegistryItem {
 	return &schema.RegistryItem{
-		Name:  "ibm_cos_bucket",
-		RFunc: newIbmCosBucket,
+		Name:                "ibm_cos_bucket",
+		RFunc:               newIbmCosBucket,
+		ReferenceAttributes: []string{"resource_instance_id"},
 	}
 }
 
+func getLocation(d *schema.ResourceData) string {
+	if d.Get("region_location").Type != gjson.Null {
+		return d.Get("region_location").String()
+	}
+	if d.Get("single_site_location").Type != gjson.Null {
+		return d.Get("single_site_location").String()
+	}
+	if d.Get("cross_region_location").Type != gjson.Null {
+		return d.Get("cross_region_location").String()
+	}
+	return ""
+}
+
 func newIbmCosBucket(d *schema.ResourceData, u *schema.UsageData) *schema.Resource {
-	region := d.Get("region").String()
+	l := getLocation(d)
+
+	refs := d.References("resource_instance_id")
+
+	fmt.Println("refs: ", refs)
+
 	r := &ibm.IbmCosBucket{
-		Address: d.Address,
-		Region:  region,
+		Address:      d.Address,
+		Region:       d.Get("region").String(),
+		Location:     l,
+		StorageClass: d.Get("storage_class").String(),
 	}
 	r.PopulateUsage(u)
 
