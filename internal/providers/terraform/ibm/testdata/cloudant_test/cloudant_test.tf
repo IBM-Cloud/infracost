42b21f7667efb79f029d9f61d50d4a83e3a0bb73
Tamas Csonka <tamas.csonka@ibm.com>
2022-08-09 12:17:42 +0200

    Adds cloudant terraform configuration for basic test case. Write first output of the test to the .golden file.

--- internal/providers/terraform/ibm/testdata/cloudant_test/cloudant_test.tf
+++ internal/providers/terraform/ibm/testdata/cloudant_test/cloudant_test.tf
@@ -1,1 +1,34 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "~> 1.40.0"
+    }
+  }
+}
 
+provider "ibm" {
+    region = "us-south"
+}
+
+resource "ibm_cloudant" "cloudant" {
+  name     = "test-cloudant"
+  location = "us-south"
+  plan     = "standard"
+
+  legacy_credentials  = true
+  include_data_events = false
+  capacity            = 1
+  enable_cors         = true
+
+  cors_config {
+    allow_credentials = false
+    origins           = ["https://example.com"]
+  }
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
