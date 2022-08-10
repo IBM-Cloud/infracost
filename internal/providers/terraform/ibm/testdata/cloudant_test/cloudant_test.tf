69d903b8b8574c7515644642c5551283105651a2
Tamas Csonka <tamas.csonka@ibm.com>
2022-08-10 12:37:52 +0200

    Add lite test resource

--- internal/providers/terraform/ibm/testdata/cloudant_test/cloudant_test.tf
+++ internal/providers/terraform/ibm/testdata/cloudant_test/cloudant_test.tf
@@ -11,10 +11,32 @@ provider "ibm" {
     region = "us-south"
 }
 
-resource "ibm_cloudant" "cloudant" {
-  name     = "test-cloudant"
+resource "ibm_cloudant" "standard_cloudant" {
+  name     = "standard-cloudant"
   location = "us-south"
   plan     = "standard"
+
+  legacy_credentials  = true
+  include_data_events = false
+  capacity            = 1
+  enable_cors         = true
+
+  cors_config {
+    allow_credentials = false
+    origins           = ["https://example.com"]
+  }
+
+  timeouts {
+    create = "15m"
+    update = "15m"
+    delete = "15m"
+  }
+}
+
+resource "ibm_cloudant" "lite_cloudant" {
+  name     = "lite-cloudant"
+  location = "us-south"
+  plan     = "lite"
 
   legacy_credentials  = true
   include_data_events = false
