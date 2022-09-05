c771743bb2f478c482ce79e4fd88b948103a2709
Balázs Marján <balazs.marjan@ibm.com>
2022-09-05 12:14:01 +0200

    fix: add gpu pricing to is_instance

--- internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
+++ internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
@@ -28,10 +28,48 @@ resource "ibm_is_ssh_key" "testSshKey" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "ibm_is_instance" "testInstance" {
+resource "ibm_is_instance" "testBalancedInstance" {
+  name    = "test-instance-1"
+  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  profile = "bx2d-32x128"
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  vpc  = ibm_is_vpc.testVpc.id
+  zone = "us-south-1"
+  keys = [ibm_is_ssh_key.testSshKey.id]
+}
+
+resource "ibm_is_instance" "testComputeInstance" {
   name    = "test-instance-1"
   image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
   profile = "cx2-2x4"
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  network_interfaces {
+    name   = "eth1"
+    subnet = ibm_is_subnet.testSubnet.id
+  }
+
+  vpc  = ibm_is_vpc.testVpc.id
+  zone = "us-south-1"
+  keys = [ibm_is_ssh_key.testSshKey.id]
+}
+
+resource "ibm_is_instance" "testGpuInstance" {
+  name    = "test-instance-1"
+  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  profile = "gx2-16x128x2v100"
 
   primary_network_interface {
     subnet = ibm_is_subnet.testSubnet.id
