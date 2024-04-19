db2e7bfd33105ff1e56e25e353146096c0d9bfa0
Luisa Rojas <luisarojas@ibm.com>
2024-04-19 17:11:44 -0400

    Add Documents cost component

--- internal/resources/ibm/resource_instance_discovery.go
+++ internal/resources/ibm/resource_instance_discovery.go
@@ -2,13 +2,14 @@ package ibm
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/infracost/infracost/internal/schema"
 	"github.com/shopspring/decimal"
 )
 
-var ENTERPRISE_PLAN_PROGRAMMATIC_NAME string = "enterprise"
-var PLUS_PLAN_PROGRAMMATIC_NAME string = "plus"
+const ENTERPRISE_PLAN_PROGRAMMATIC_NAME string = "enterprise"
+const PLUS_PLAN_PROGRAMMATIC_NAME string = "plus"
 
 /*
  * Plus = 'plus'
@@ -50,19 +51,19 @@ func GetWDCostComponents(r *ResourceInstance) []*schema.CostComponent {
  */
 func WDInstanceCostComponent(r *ResourceInstance) *schema.CostComponent {
 
-	var quantity *decimal.Decimal
 	var instances_unit_name string
-
-	if r.WD_Instance != nil {
-		quantity = decimalPtr(decimal.NewFromFloat(*r.WD_Instance))
-	} else {
-		quantity = decimalPtr(decimal.NewFromInt(1))
-	}
+	var quantity *decimal.Decimal
 
 	if r.Plan == PLUS_PLAN_PROGRAMMATIC_NAME {
 		instances_unit_name = "PLUS_SERVICE_INSTANCES_PER_MONTH"
 	} else {
 		instances_unit_name = "ENTERPRISE_SERVICE_INSTANCES_PER_MONTH"
+	}
+
+	if r.WD_Instance != nil {
+		quantity = decimalPtr(decimal.NewFromFloat(*r.WD_Instance))
+	} else {
+		quantity = decimalPtr(decimal.NewFromInt(1))
 	}
 
 	return &schema.CostComponent{
@@ -90,8 +91,46 @@ func WDInstanceCostComponent(r *ResourceInstance) *schema.CostComponent {
  * - Enterprise: $USD/documents/month. Includes 100,000 documents per month; $USD for every additional 1,000 documents.
  */
 func WDMonthlyDocumentsCostComponent(r *ResourceInstance) *schema.CostComponent {
+
+	var documents_additional_range int = 1000 // Additional cost for every 1000 over the included amount of documents
+	var documents_included int
+	var documents_unit_name string
 	var quantity *decimal.Decimal
 
+	if r.Plan == PLUS_PLAN_PROGRAMMATIC_NAME {
+		documents_unit_name = "PLUS_DOCUMENTS_TOTAL"
+		documents_included = 10000
+	} else {
+		documents_unit_name = "ENTERPRISE_DOCUMENTS_TOTAL"
+		documents_included = 100000
+	}
+
+	if r.WD_Document != nil {
+
+		additional_documents := *r.WD_Document - float64(documents_included)
+
+		if additional_documents > 0 {
+			quantity = decimalPtr(decimal.NewFromFloat(math.Ceil(additional_documents / float64(documents_additional_range))))
+		}
+	}
+
+	return &schema.CostComponent{
+		Name:            "Additional Monthly Documents",
+		Unit:            "1K documents",
+		UnitMultiplier:  decimal.NewFromInt(1),
+		MonthlyQuantity: quantity,
+		ProductFilter: &schema.ProductFilter{
+			VendorName: strPtr("ibm"),
+			Region:     strPtr(r.Location),
+			Service:    &r.Service,
+			AttributeFilters: []*schema.AttributeFilter{
+				{Key: "planName", Value: &r.Plan},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr(documents_unit_name),
+		},
+	}
 }
 
 /*
@@ -102,6 +141,8 @@ func WDMonthlyDocumentsCostComponent(r *ResourceInstance) *schema.CostComponent 
 func WDMonthlyQueriesCostComponent(r *ResourceInstance) *schema.CostComponent {
 	var quantity *decimal.Decimal
 
+	// TODO: Determine quantity
+	// Unit name: "GRADUATED_PRICE_QUERIES_PER_MONTH"
 }
 
 /*
@@ -111,6 +152,7 @@ func WDMonthlyQueriesCostComponent(r *ResourceInstance) *schema.CostComponent {
 func WDMonthlyCustomModelsCostComponent(r *ResourceInstance) *schema.CostComponent {
 	var quantity *decimal.Decimal
 
+	// Unit name: "CUSTOM_MODELS_PER_MONTH"
 }
 
 /*
@@ -120,4 +162,5 @@ func WDMonthlyCustomModelsCostComponent(r *ResourceInstance) *schema.CostCompone
 func WDMonthlyCollectionsCostComponent(r *ResourceInstance) *schema.CostComponent {
 	var quantity *decimal.Decimal
 
+	// Unit name: "ENTERPRISE_COLLECTIONS_TOTAL"
 }
