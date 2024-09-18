011ccc8431252a432c278b0749a3f499dde2f908
hlem@ca.ibm.com <hlem@ca.ibm.com>
2024-09-18 09:00:51 -0400

    fix a couple

--- internal/hcl/funcs/collection.go
+++ internal/hcl/funcs/collection.go
@@ -3,7 +3,6 @@ package funcs
 import (
 	"errors"
 	"fmt"
-	"math/big"
 	"sort"
 
 	"github.com/zclconf/go-cty/cty"
@@ -477,71 +476,6 @@ var OneFunc = function.New(&function.Spec{
 	},
 })
 
-// SumFunc constructs a function that returns the sum of all
-// numbers provided in a list
-var SumFunc = function.New(&function.Spec{
-	Params: []function.Parameter{
-		{
-			Name: "list",
-			Type: cty.DynamicPseudoType,
-		},
-	},
-	Type: function.StaticReturnType(cty.Number),
-	Impl: func(args []cty.Value, retType cty.Type) (ret cty.Value, err error) {
-
-		if !args[0].CanIterateElements() {
-			return cty.NilVal, function.NewArgErrorf(0, "cannot sum noniterable")
-		}
-
-		if args[0].LengthInt() == 0 { // Easy path
-			return cty.NilVal, function.NewArgErrorf(0, "cannot sum an empty list")
-		}
-
-		arg := args[0].AsValueSlice()
-		ty := args[0].Type()
-
-		if !ty.IsListType() && !ty.IsSetType() && !ty.IsTupleType() {
-			return cty.NilVal, function.NewArgErrorf(0, fmt.Sprintf("argument must be list, set, or tuple. Received %s", ty.FriendlyName()))
-		}
-
-		if !args[0].IsWhollyKnown() {
-			return cty.UnknownVal(cty.Number), nil
-		}
-
-		// big.Float.Add can panic if the input values are opposing infinities,
-		// so we must catch that here in order to remain within
-		// the cty Function abstraction.
-		defer func() {
-			if r := recover(); r != nil {
-				if _, ok := r.(big.ErrNaN); ok {
-					ret = cty.NilVal
-					err = fmt.Errorf("can't compute sum of opposing infinities")
-				} else {
-					// not a panic we recognize
-					panic(r)
-				}
-			}
-		}()
-
-		s := arg[0]
-		if s.IsNull() {
-			return cty.NilVal, function.NewArgErrorf(0, "argument must be list, set, or tuple of number values")
-		}
-		for _, v := range arg[1:] {
-			if v.IsNull() {
-				return cty.NilVal, function.NewArgErrorf(0, "argument must be list, set, or tuple of number values")
-			}
-			v, err = convert.Convert(v, cty.Number)
-			if err != nil {
-				return cty.NilVal, function.NewArgErrorf(0, "argument must be list, set, or tuple of number values")
-			}
-			s = s.Add(v)
-		}
-
-		return s, nil
-	},
-})
-
 // TransposeFunc constructs a function that takes a map of lists of strings and
 // swaps the keys and values to produce a new map of lists of strings.
 var TransposeFunc = function.New(&function.Spec{
@@ -692,11 +626,6 @@ func Matchkeys(values, keys, searchset cty.Value) (cty.Value, error) {
 // if given a zero-element list..
 func One(list cty.Value) (cty.Value, error) {
 	return OneFunc.Call([]cty.Value{list})
-}
-
-// Sum adds numbers in a list, set, or tuple
-func Sum(list cty.Value) (cty.Value, error) {
-	return SumFunc.Call([]cty.Value{list})
 }
 
 // Transpose takes a map of lists of strings and swaps the keys and values to
