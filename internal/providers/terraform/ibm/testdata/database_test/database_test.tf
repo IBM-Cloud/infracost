294a959b842118e4b831d606ca4e220e443bc42e
Luisa Rojas <luisarojas@ibm.com>
2024-09-22 15:49:39 -0400

    Update Terraform tests for postgresql and elasticsearch

--- internal/providers/terraform/ibm/testdata/database_test/database_test.tf
+++ internal/providers/terraform/ibm/testdata/database_test/database_test.tf
@@ -10,22 +10,24 @@ provider "ibm" {
   region = "us-south"
 }
 
-resource "ibm_database" "test_db1" {
-  name     = "demo-postgres"
+# -------------------------------------------
+# POSTGRES
+# -------------------------------------------
+
+resource "ibm_database" "postgresql_standard_flavour" {
+  name     = "postgres-standard-flavour"
   service  = "databases-for-postgresql"
   plan     = "standard"
-  location = "eu-gb"
+  location = "us-south"
 
-  group {
+  group { # Note: "memory" not allowed when host_flavor is set
     group_id = "member"
-    memory {
-      allocation_mb = 12288
+    host_flavor {
+      id = "m3c.30x240.encrypted"
     }
+
     disk {
-      allocation_mb = 131072
-    }
-    cpu {
-      allocation_count = 3
+      allocation_mb = 4194304
     }
   }
   configuration = <<CONFIGURATION
@@ -35,19 +37,22 @@ resource "ibm_database" "test_db1" {
   CONFIGURATION
 }
 
-resource "ibm_database" "test_db2" {
-  name     = "demo-postgres2"
+resource "ibm_database" "postgresql_standard" {
+  name     = "postgres-standard"
   service  = "databases-for-postgresql"
   plan     = "standard"
-  location = "eu-gb"
+  location = "us-south"
 
   group {
     group_id = "member"
     memory {
-      allocation_mb = 15360
+      allocation_mb = 114688
     }
-    members {
-      allocation_count = 4
+    disk {
+      allocation_mb = 4194304
+    }
+    cpu {
+      allocation_count = 28
     }
   }
   configuration = <<CONFIGURATION
@@ -57,76 +62,138 @@ resource "ibm_database" "test_db2" {
   CONFIGURATION
 }
 
-resource "ibm_database" "test_es_enterprise_db1" {
-  name     = "demo-es-enterprise"
-  service  = "databases-for-elasticsearch"
+# 'enterprise' plan is no longer officially supported by IBM Cloud for postgresql, but still allowed by Terraform
+resource "ibm_database" "postgresql_enterprise" {
+  name     = "postgres-enterprise"
+  service  = "databases-for-postgresql"
   plan     = "enterprise"
-  location = "eu-gb"
+  location = "us-south"
 
   group {
     group_id = "member"
     memory {
-      allocation_mb = 12288
+      allocation_mb = 114688
     }
     disk {
-      allocation_mb = 131072
+      allocation_mb = 4194304
     }
     cpu {
-      allocation_count = 3
+      allocation_count = 28
     }
   }
-}
-
-resource "ibm_database" "test_es_enterprise_db2" {
-  name     = "demo-es-enterprise2"
-  service  = "databases-for-elasticsearch"
-  plan     = "enterprise"
-  location = "eu-gb"
-
-  group {
-    group_id = "member"
-    members {
-      allocation_count = 4
-    }
-    memory {
-      allocation_mb = 15360
-    }
+  configuration = <<CONFIGURATION
+  {
+    "max_connections": 400
   }
+  CONFIGURATION
 }
 
-resource "ibm_database" "test_es_platinum_db1" {
-  name     = "demo-es-platinum"
-  service  = "databases-for-elasticsearch"
+# 'platinum' plan is no longer officially supported by IBM Cloud for postgresql, but still allowed by Terraform
+resource "ibm_database" "postgresql_platinum" {
+  name     = "postgres-platinum"
+  service  = "databases-for-postgresql"
   plan     = "platinum"
-  location = "eu-gb"
+  location = "us-south"
 
   group {
     group_id = "member"
     memory {
-      allocation_mb = 12288
+      allocation_mb = 114688
     }
     disk {
-      allocation_mb = 131072
+      allocation_mb = 4194304
     }
     cpu {
-      allocation_count = 3
+      allocation_count = 28
+    }
+  }
+  configuration = <<CONFIGURATION
+  {
+    "max_connections": 400
+  }
+  CONFIGURATION
+}
+
+# -------------------------------------------
+# ELASTICSEARCH
+# -------------------------------------------
+
+resource "ibm_database" "elasticsearch_standard" {
+  name     = "elasticsearch-standard"
+  service  = "databases-for-elasticsearch"
+  plan     = "standard"
+  location = "us-south"
+
+  group {
+    group_id = "member"
+    memory {
+      allocation_mb = 114688
+    }
+    disk {
+      allocation_mb = 4194304
+    }
+    cpu {
+      allocation_count = 28
     }
   }
 }
 
-resource "ibm_database" "test_es_platinum_db2" {
-  name     = "demo-es-platinum2"
+resource "ibm_database" "elasticsearch_standard_flavour" {
+  name     = "elasticsearch-standard-flavour"
+  service  = "databases-for-elasticsearch"
+  plan     = "standard"
+  location = "us-south"
+
+  group { # Note: "memory" not allowed when host_flavor is set
+    group_id = "member"
+
+    host_flavor {
+      id = "m3c.30x240.encrypted"
+    }
+    disk {
+      allocation_mb = 4194304
+    }
+  }
+}
+
+resource "ibm_database" "elasticsearch_platinum" {
+  name     = "elasticsearch-platinum"
   service  = "databases-for-elasticsearch"
   plan     = "platinum"
-  location = "eu-gb"
+  location = "us-south"
 
   group {
     group_id = "member"
-    members {
-      allocation_count = 4
-    }
+
     memory {
-      allocation_mb = 2048
+      allocation_mb = 114688
+    }
+    disk {
+      allocation_mb = 4194304
+    }
+    cpu {
+      allocation_count = 28
+    }
+  }
+}
+
+resource "ibm_database" "elasticsearch_enterprise" {
+  name     = "elasticsearch-enterprise"
+  service  = "databases-for-elasticsearch"
+  plan     = "enterprise"
+  location = "us-south"
+
+  group {
+    group_id = "member"
+
+    memory {
+      allocation_mb = 114688
+    }
+    disk {
+      allocation_mb = 4194304
+    }
+    cpu {
+      allocation_count = 28
     }
   }
 }
