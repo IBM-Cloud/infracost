adad331dd7fbee0d5cd68d984e32f2456d7032c9
Luisa Rojas <luisarojas@ibm.com>
2024-09-23 01:50:55 -0400

    Re-write cost components for postgresql

--- internal/resources/ibm/database_postgresql.go
+++ internal/resources/ibm/database_postgresql.go
@@ -1,116 +1,128 @@
 package ibm
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/infracost/infracost/internal/schema"
 	"github.com/shopspring/decimal"
 )
 
-func PostgresRAMCostComponent(r *Database) *schema.CostComponent {
-	var R decimal.Decimal
-	if r.PostgreSQL_Ram != nil {
-		R = ConvertMBtoGB(decimal.NewFromInt(*r.PostgreSQL_Ram))
-	} else { // set the default
-		R = decimal.NewFromInt(1)
-	}
-	var m decimal.Decimal
-	if r.PostgreSQL_Members != nil {
-		m = decimal.NewFromInt(*r.PostgreSQL_Members)
-	} else { // set the default
-		m = decimal.NewFromInt(2)
-	}
+func GetPostgresCostComponents(r *Database) []*schema.CostComponent {
 
-	cost := R.Mul(m)
-
-	costComponent := schema.CostComponent{
-		Name:            "RAM",
-		Unit:            "GB-RAM",
-		MonthlyQuantity: &cost,
-		UnitMultiplier:  decimal.NewFromInt(1),
-		ProductFilter: &schema.ProductFilter{
-			VendorName:    strPtr("ibm"),
-			Region:        strPtr(r.Location),
-			Service:       strPtr("databases-for-postgresql"),
-			ProductFamily: strPtr("service"),
-		},
-		PriceFilter: &schema.PriceFilter{
-			Unit: strPtr("GIGABYTE_MONTHS_RAM"),
-		},
+	if r.Flavor != "" {
+		return []*schema.CostComponent{
+			PostgresHostFlavorComponent(r),
+			PostgresDiskCostComponent(r),
+		}
+	} else {
+		return []*schema.CostComponent{
+			PostgresRAMCostComponent(r),
+			PostgresDiskCostComponent(r),
+			PostgresVirtualProcessorCoreCostComponent(r),
+		}
 	}
-	return &costComponent
 }
 
-func PostgresDiskCostComponent(r *Database) *schema.CostComponent {
-	var d decimal.Decimal
-	if r.PostgreSQL_Disk != nil {
-		d = ConvertMBtoGB(decimal.NewFromInt(*r.PostgreSQL_Disk))
-	} else { // set the default
-		d = decimal.NewFromInt(5)
-	}
-	var m decimal.Decimal
-	if r.PostgreSQL_Members != nil {
-		m = decimal.NewFromInt(*r.PostgreSQL_Members)
-	} else { // set the default
-		m = decimal.NewFromInt(2)
-	}
-
-	cost := d.Mul(m)
-
-	costComponent := schema.CostComponent{
-		Name:            "Disk",
-		Unit:            "GB-DISK",
-		MonthlyQuantity: &cost,
-		UnitMultiplier:  decimal.NewFromInt(1),
+func PostgresVirtualProcessorCoreCostComponent(r *Database) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Virtual Processor Cores (%s members)", strconv.FormatInt(r.Members, 10)),
+		Unit:            "CPU",
+		UnitMultiplier:  decimal.NewFromInt(1), // Final quantity for this cost component will be divided by this amount
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(r.CPU * r.Members)),
 		ProductFilter: &schema.ProductFilter{
 			VendorName:    strPtr("ibm"),
 			Region:        strPtr(r.Location),
 			Service:       strPtr("databases-for-postgresql"),
 			ProductFamily: strPtr("service"),
-		},
-		PriceFilter: &schema.PriceFilter{
-			Unit: strPtr("GIGABYTE_MONTHS_DISK"),
-		},
-	}
-	return &costComponent
-}
-
-func PostgresCoreCostComponent(r *Database) *schema.CostComponent {
-	var c decimal.Decimal
-	if r.PostgreSQL_Core != nil {
-		c = decimal.NewFromInt(*r.PostgreSQL_Core)
-	} else { // set the default
-		c = decimal.NewFromInt(0)
-	}
-	var m decimal.Decimal
-	if r.PostgreSQL_Members != nil {
-		m = decimal.NewFromInt(*r.PostgreSQL_Members)
-	} else { // set the default
-		m = decimal.NewFromInt(2)
-	}
-
-	cost := c.Mul(m)
-
-	costComponent := schema.CostComponent{
-		Name:            "Core",
-		Unit:            "Virtual Processor Core",
-		MonthlyQuantity: &cost,
-		UnitMultiplier:  decimal.NewFromInt(1),
-		ProductFilter: &schema.ProductFilter{
-			VendorName:    strPtr("ibm"),
-			Region:        strPtr(r.Location),
-			Service:       strPtr("databases-for-postgresql"),
-			ProductFamily: strPtr("service"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{
+					Key: "planName", Value: strPtr(r.Plan),
+				},
+			},
 		},
 		PriceFilter: &schema.PriceFilter{
 			Unit: strPtr("VIRTUAL_PROCESSOR_CORES"),
 		},
 	}
-	return &costComponent
 }
 
-func GetPostgresCostComponents(r *Database) []*schema.CostComponent {
-	return []*schema.CostComponent{
-		PostgresRAMCostComponent(r),
-		PostgresDiskCostComponent(r),
-		PostgresCoreCostComponent(r),
+func PostgresRAMCostComponent(r *Database) *schema.CostComponent {
+
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("RAM (%s members)", strconv.FormatInt(r.Members, 10)),
+		Unit:            "GB",
+		UnitMultiplier:  decimal.NewFromInt(1),                                         // Final quantity for this cost component will be divided by this amount
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt((r.Memory * r.Members) / 1024)), // Convert to GB
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("ibm"),
+			Region:        strPtr(r.Location),
+			Service:       strPtr("databases-for-postgresql"),
+			ProductFamily: strPtr("service"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{
+					Key: "planName", Value: strPtr(r.Plan),
+				},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr("GIGABYTE_MONTHS_RAM_NEW"),
+		},
+	}
+}
+
+func PostgresDiskCostComponent(r *Database) *schema.CostComponent {
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Disk (%s members)", strconv.FormatInt(r.Members, 10)),
+		Unit:            "GB",
+		UnitMultiplier:  decimal.NewFromInt(1),                                       // Final quantity for this cost component will be divided by this amount
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt((r.Disk * r.Members) / 1024)), // Convert to GB
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("ibm"),
+			Region:        strPtr(r.Location),
+			Service:       strPtr("databases-for-postgresql"),
+			ProductFamily: strPtr("service"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{
+					Key: "planName", Value: strPtr(r.Plan),
+				},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr("GIGABYTE_MONTHS_DISK"),
+		},
+	}
+}
+
+func PostgresHostFlavorComponent(r *Database) *schema.CostComponent {
+
+	CostUnit := map[string]string{
+		"b3c.4x16.encrypted":   "HOST_FOUR_SIXTEEN",
+		"b3c.8x32.encrypted":   "HOST_EIGHT_THIRTYTWO",
+		"m3c.8x64.encrypted":   "HOST_EIGHT_SIXTYFOUR",
+		"b3c.16x64.encrypted":  "HOST_SIXTEEN_SIXTYFOUR",
+		"b3c.32x128.encrypted": "HOST_THIRTYTWO_ONEHUNDREDTWENTYEIGHT",
+		"m3c.30x240.encrypted": "HOST_THIRTY_TWOHUNDREDFORTY",
+	}
+
+	return &schema.CostComponent{
+		Name:            fmt.Sprintf("Host Flavor (%s members, %s)", strconv.FormatInt(r.Members, 10), r.Flavor),
+		Unit:            "Flavor",
+		UnitMultiplier:  decimal.NewFromInt(1), // Final quantity for this cost component will be divided by this amount
+		MonthlyQuantity: decimalPtr(decimal.NewFromInt(r.Members)),
+		ProductFilter: &schema.ProductFilter{
+			VendorName:    strPtr("ibm"),
+			Region:        strPtr(r.Location),
+			Service:       strPtr("databases-for-postgresql"),
+			ProductFamily: strPtr("service"),
+			AttributeFilters: []*schema.AttributeFilter{
+				{
+					Key: "planName", Value: strPtr(r.Plan),
+				},
+			},
+		},
+		PriceFilter: &schema.PriceFilter{
+			Unit: strPtr(CostUnit[r.Flavor]),
+		},
 	}
 }
