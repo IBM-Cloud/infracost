2e00abf3ac29b3eadcbbfd04bfafb62030d5a171
Luisa Rojas <r.luisaf@gmail.com>
2024-09-27 15:36:32 -0400

    Update Terraform tests for local run

--- internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
+++ internal/providers/terraform/ibm/testdata/is_instance_test/is_instance_test.tf
@@ -4,6 +4,9 @@ terraform {
     ibm = {
       source = "IBM-Cloud/ibm"
     }
+    random = {
+      source = "hashicorp/random"
+    }
   }
 }
 
@@ -11,29 +14,45 @@ provider "ibm" {
   region = "us-south"
 }
 
+# Access random string generated with random_string.unique_identifier.result
+resource "random_string" "unique_identifier" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
+resource "ibm_resource_group" "resource_group" {
+  name = "${random_string.unique_identifier.result}-rg"
+}
+
 resource "ibm_is_vpc" "vpc" {
-  name = "test-vpc"
+  name           = "${random_string.unique_identifier.result}-vpc"
+  resource_group = ibm_resource_group.resource_group.id
 }
 
 resource "ibm_is_subnet" "subnet" {
-  name            = "test-subnet"
+  name            = "${random_string.unique_identifier.result}-subnet"
+  ipv4_cidr_block = "10.240.0.0/24"
+  resource_group  = ibm_resource_group.resource_group.id
   vpc             = ibm_is_vpc.vpc.id
   zone            = "us-south-1"
-  ipv4_cidr_block = "10.240.0.0/24"
 }
 
 resource "ibm_is_ssh_key" "ssh_key" {
-  name       = "test-ssh"
-  public_key = file("~/.ssh/id_rsa.pub")
+  name           = "${random_string.unique_identifier.result}-ssh"
+  public_key     = file("~/.ssh/id_ed25519.pub")
+  resource_group = ibm_resource_group.resource_group.id
+  type           = "ed25519"
 }
 
 resource "ibm_is_instance" "vsi" {
-  name    = "vsi-instance"
-  image   = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
-  profile = "cx3d-2x5"
-  keys    = [ibm_is_ssh_key.ssh_key.id]
-  vpc     = ibm_is_vpc.vpc.id
-  zone    = "us-south-1"
+  name           = "${random_string.unique_identifier.result}-vsi-instance"
+  image          = "r006-f137ea64-0d27-4d81-afe0-353fd0557e81"
+  keys           = [ibm_is_ssh_key.ssh_key.id]
+  profile        = "cx3d-2x5"
+  resource_group = ibm_resource_group.resource_group.id
+  vpc            = ibm_is_vpc.vpc.id
+  zone           = "us-south-1"
   primary_network_interface {
     subnet = ibm_is_subnet.subnet.id
   }
@@ -41,4 +60,10 @@ resource "ibm_is_instance" "vsi" {
     name   = "eth1"
     subnet = ibm_is_subnet.subnet.id
   }
+}
+
+locals {
+  profiles = [
+
+  ]
 }
