072326f93b5932322b944deb5e23c21c419f6c3a
Luisa Rojas <r.luisaf@gmail.com>
2024-11-05 18:06:18 -0500

    Fix ProductFilter for Watson Assistant resource

--- internal/resources/ibm/resource_instance_conversation.go
+++ internal/resources/ibm/resource_instance_conversation.go
@@ -31,6 +31,14 @@ func GetWACostComponents(r *ResourceInstance) []*schema.CostComponent {
 			Name:            "Lite plan",
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("ibm"),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{
@@ -41,6 +49,14 @@ func GetWACostComponents(r *ResourceInstance) []*schema.CostComponent {
 			Name:            "Trial",
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("ibm"),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{
@@ -48,9 +64,17 @@ func GetWACostComponents(r *ResourceInstance) []*schema.CostComponent {
 		}
 	} else {
 		costComponent := schema.CostComponent{
-			Name:            fmt.Sprintf("Plan %s with customized pricing", r.Plan),
+			Name:            fmt.Sprintf("Plan %s not found", r.Plan),
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("ibm"),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{
