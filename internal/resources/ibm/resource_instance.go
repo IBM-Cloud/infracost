52b5aff5688dd319270c86c5e12b7689c4491c32
Luisa Rojas <r.luisaf@gmail.com>
2024-11-05 16:18:36 -0500

    Fix ProductFilter for Continuous Delivery resource

--- internal/resources/ibm/resource_instance.go
+++ internal/resources/ibm/resource_instance.go
@@ -663,6 +663,14 @@ func GetContinuousDeliveryCostComponenets(r *ResourceInstance) []*schema.CostCom
 			Name:            "Lite plan",
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("ibm"),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{costComponent}
