6ad043a216b9c017bc8d4e9dd25578d91565f4d6
Luisa Rojas <r.luisaf@gmail.com>
2024-11-05 17:25:56 -0500

    Fix ProductFilter for Event Streams resource

--- internal/resources/ibm/resource_instance_messagehub.go
+++ internal/resources/ibm/resource_instance_messagehub.go
@@ -37,14 +37,30 @@ func GetEventStreamsCostComponents(r *ResourceInstance) []*schema.CostComponent 
 			Name:            "Lite plan",
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr("ibm"),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{costComponent}
 	} else {
 		costComponent := schema.CostComponent{
-			Name:            fmt.Sprintf("Plan %s with customized pricing", r.Plan),
+			Name:            fmt.Sprintf("Plan %s not found", r.Plan),
 			UnitMultiplier:  decimal.NewFromInt(1),
 			MonthlyQuantity: decimalPtr(decimal.NewFromInt(1)),
+			ProductFilter: &schema.ProductFilter{
+				VendorName: strPtr(""),
+				Region:     strPtr(r.Location),
+				Service:    &r.Service,
+				AttributeFilters: []*schema.AttributeFilter{
+					{Key: "planName", Value: &r.Plan},
+				},
+			},
 		}
 		costComponent.SetCustomPrice(decimalPtr(decimal.NewFromInt(0)))
 		return []*schema.CostComponent{
