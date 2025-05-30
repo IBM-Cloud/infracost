49f2d8109b6cfe15641408d856dc4534460de2ff
Jack.Cameron@IBM.com <Jack.Cameron@IBM.com>
2025-05-30 14:13:52 -0400

    add logs component

--- internal/resources/ibm/resource_instance.go
+++ internal/resources/ibm/resource_instance.go
@@ -110,6 +110,7 @@ type ResourceInstance struct {
 	EventStreams_Terabytes                    *float64 `infracost_usage:"messagehub_qty_terabytes"`
 	// Event Notifications
 	EventNotifications_InboundIngestedEvents *float64 `infracost_usage:"event-notifications_MILLION_INGESTED_EVENTS"`
+	Logs_Hour 								  *float64 `infracost_usage:"logs_hour"`
 }
 
 type ResourceCostComponentsFunc func(*ResourceInstance) []*schema.CostComponent
@@ -202,6 +203,7 @@ var ResourceInstanceCostMap map[string]ResourceCostComponentsFunc = map[string]R
 	"secrets-manager":         GetSecretsManagerCostComponents,
 	"sysdig-monitor":          GetSysdigCostComponenets,
 	"sysdig-secure":           GetSCCWPCostComponents,
+	"logs":					   GetLogsCostComponents,
 }
 
 func KMSKeyVersionsFreeCostComponent(r *ResourceInstance) *schema.CostComponent {
