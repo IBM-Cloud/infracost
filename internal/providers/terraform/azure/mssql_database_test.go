736c2774b090fe36435c3b07f0e2e53e0d7ca11d
Michael Lu Chen <Michael.Chen1@ibm.com>
2025-06-24 16:35:56 -0400

    Comment out HCL function calls for cosmodb and mysql in azure

--- internal/providers/terraform/azure/mssql_database_test.go
+++ internal/providers/terraform/azure/mssql_database_test.go
@@ -16,13 +16,13 @@ func TestMSSQLDatabase(t *testing.T) {
 	tftest.GoldenFileResourceTestsWithOpts(t, "mssql_database_test", opts)
 }
 
-func TestMSSQLDatabaseWithBlankLocation(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode")
-	}
+// func TestMSSQLDatabaseWithBlankLocation(t *testing.T) {
+// 	if testing.Short() {
+// 		t.Skip("skipping test in short mode")
+// 	}
 
-	opts := tftest.DefaultGoldenFileOptions()
-	opts.CaptureLogs = true
+// 	opts := tftest.DefaultGoldenFileOptions()
+// 	opts.CaptureLogs = true
 
-	tftest.GoldenFileHCLResourceTestsWithOpts(t, "mssql_database_test_with_blank_location", opts)
-}
+// 	tftest.GoldenFileHCLResourceTestsWithOpts(t, "mssql_database_test_with_blank_location", opts)
+// }
