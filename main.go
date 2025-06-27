364a5df0d50149b6dcab16ff0e42c0879709e13c
jack-cameron16 <Jack.Cameron@ibm.com>
2025-06-27 11:22:16 -0400

    Issue #352: Fix is_instance image cost not showing (#281)
    * initial commit
    
    * get pricing in for SUSE
    
    * change image attr.
    
    * add image id map
    
    * remove identifyos func
    
    * fix sql licencecfunc
    
    * added sles for sap + test tf fix
    
    * add test sles
    
    * make imageMap based on images.json
    
    * change path variable
    
    * use go:embed

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import _ "embed"
 //go:embed infracost-usage-example.yml
 var referenceUsageFileContents []byte
 
+//go:embed images.json
+var imageFileContents []byte
+
 func GetReferenceUsageFileContents() *[]byte {
 	return &referenceUsageFileContents
+}
+
+func GetImageFileContent() *[]byte {
+	return &imageFileContents
 }
